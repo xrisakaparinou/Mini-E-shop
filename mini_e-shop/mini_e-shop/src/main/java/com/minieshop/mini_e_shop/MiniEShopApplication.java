48afafca0bf31730a61bdae5613511e00dc596b8
xrisakaparinou <xrisakaparinou@gmail.com>
2025-05-25 21:03:29 +0300

    initial commit

---
+++ mini_e-shop/mini_e-shop/src/main/java/com/minieshop/mini_e_shop/MiniEShopApplication.java
@@ -0,0 +1,13 @@
+package com.minieshop.mini_e_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniEShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniEShopApplication.class, args);
+	}
+
+}
---
+++ mini_e-shop/mini_e-shop/src/test/java/com/minieshop/mini_e_shop/MiniEShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minieshop.mini_e_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniEShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
